f20be47f8e4d7946255137b2f3031431e628b54e
Your Name <Your Name>
2025-07-04 01:05:06 +0300

    2

--- android/app/src/main/java/com/tmody/QTM/MainActivity.kt
+++ android/app/src/main/java/com/tmody/QTM/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tmody.QMC
+package com.tmody.QTM
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/tmody/QTM/MainApplication.kt
+++ android/app/src/main/java/com/tmody/QTM/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.tmody.QMC
+package com.tmody.QTM
 
 import android.app.Application
 import android.content.res.Configuration
