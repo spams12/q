6627735950887e37b8c4fc377dd77314ccc76482
Your Name <you@example.com>
2025-08-30 18:43:33 +0300

    e

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,18 +8,6 @@ import { getFirestore } from "firebase/firestore";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { CommentAttachment } from "./types";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBHErhfhQHFURogL_WENFXy8ajwwQ7Zw_E",
-  authDomain: "react-57a14.firebaseapp.com",
-  projectId: "react-57a14",
-  storageBucket: "react-57a14.appspot.com",
-  messagingSenderId: "769253344130",
-  appId: "1:769253344130:web:2b7358461ebc505b19059b",
-  measurementId: "G-PS0BMKJKRW",
-  databaseURL: "https://react-57a14-default-rtdb.europe-west1.firebasedatabase.app/", 
-};
-
-
 const app = initializeApp(firebaseConfig);
 
 const auth = initializeAuth(app, {
@@ -31,8 +19,14 @@ const rtdb = getDatabase(app); // <-- Initialize and get your Realtime Database 
 const storage = getStorage(app);
 
 // This function remains unchanged
-export const uploadCommentAttachment = async (file: File, ticketId: string): Promise<CommentAttachment> => {
-  const storageRef = ref(storage, `serviceRequests/${ticketId}/comments/${file.name}`);
+export const uploadCommentAttachment = async (
+  file: File,
+  ticketId: string
+): Promise<CommentAttachment> => {
+  const storageRef = ref(
+    storage,
+    `serviceRequests/${ticketId}/comments/${file.name}`
+  );
   await uploadBytes(storageRef, file);
   const downloadURL = await getDownloadURL(storageRef);
   return {
@@ -45,4 +39,3 @@ export const uploadCommentAttachment = async (file: File, ticketId: string): Pro
 
 // Export all the services
 export { auth, db, rtdb, storage };
-
